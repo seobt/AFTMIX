49344535756d0bb8d5f0a375618117cc6e27bd8c
seobt <60186073+seobt@users.noreply.github.com>
2021-01-14 21:54:38 +0900

    Delete AFTMIX directory

--- AFTMIX/normal_mixture.m
+++
@@ -1,8 +0,0 @@
-function answer=normal_mixture(x,d,loc,variance,weight)
-
-%% univariate noraml mixture
-answer=0;
-for i=1:length(loc)
-    answer=answer+weight(i)*(d*normal_pdf(x,loc(i),variance(i))+(1-d)*(1-normcdf(x,loc(i),sqrt(variance(i)))));
-end
-
